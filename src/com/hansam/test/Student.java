b7576485cf42102bdc4c1fcfe52ea1bba044b5b8
ShiShuai <945210972@qq.com>
2015-12-08 15:46:24 +0800

    这是第二次提交
    this is a update.

---
+++ src/com/hansam/test/Student.java
@@ -0,0 +1,21 @@
+package com.hansam.test;
+
+public class Student {
+	private int stuId;
+	private String stuName;
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	
+	
+
+}
